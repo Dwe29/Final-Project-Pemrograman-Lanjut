f8ffd415c8156343521ad76f2d3115a5fe4772e6
Wahyu Dwe <wahyu5161.w5@gmail.com>
2021-12-13 07:51:43 +0700

    first

---
+++ Final Project Pemrograman Lanjut/view/LoginWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Final_Project_Pemrograman_Lanjut.view
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Final Project Pemrograman Lanjut/view/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Final_Project_Pemrograman_Lanjut.view
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

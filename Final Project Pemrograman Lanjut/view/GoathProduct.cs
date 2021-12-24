a126872e6d83ca6c544cfa33113c78ec0443535a
Wahyu Dwe <wahyu5161.w5@gmail.com>
2021-12-24 11:19:53 +0700

    add image

---
+++ Final Project Pemrograman Lanjut/view/GoathProduct.cs
@@ -0,0 +1,16 @@
+﻿namespace Final_Project_Pemrograman_Lanjut.view
+{
+    public class GoathProduct
+    {
+        public string Name { get; set; }
+        public double Value { get; set; }
+        public string Image { get; set; }
+
+        public GoathProduct(string name, double value, string image)
+        {
+            Name = name;
+            Value = value;
+            Image = image;
+        }
+    }
+}

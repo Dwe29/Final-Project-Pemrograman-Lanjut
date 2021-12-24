a4859204c922a715cb41e2d8c328033037157d98
Wahyu Dwe <wahyu5161.w5@gmail.com>
2021-12-24 11:18:16 +0700

    Fixed frame bug

---
+++ Final Project Pemrograman Lanjut/controller/ProdukControllerChicken.cs
@@ -0,0 +1,17 @@
+﻿using Final_Project_Pemrograman_Lanjut.model;
+using Final_Project_Pemrograman_Lanjut.view;
+
+namespace Final_Project_Pemrograman_Lanjut.controller
+{
+    public class ProdukControllerChicken
+    {
+        private ProdukModelChicken _model;
+        private ChickenPage _view;
+
+        public ProdukControllerChicken(ChickenPage chickenPage)
+        {
+            _model = new ProdukModelChicken();
+            _view = chickenPage;
+        }
+    }
+}

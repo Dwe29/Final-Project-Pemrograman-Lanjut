8a5cc1b2cae032e89627e57bf0831a4c146be6dc
Wahyu Dwe <wahyu5161.w5@gmail.com>
2021-12-27 14:21:32 +0700

    dont know how to show single data from database

---
+++ Final Project Pemrograman Lanjut/model/BuyProductModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+
+namespace Final_Project_Pemrograman_Lanjut.model
+{
+    public class BuyProductModel
+    {
+        private ModelTemplate _template;
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Jenis { get; set; }
+
+        public BuyProductModel()
+        {
+            _template = new ModelTemplate();
+        }
+
+        public DataSet ShowData()
+        {
+            return _template.Select("produk", "id = '"+Id+"', nama = '"+Name+"', gambar = '"+Image+"', jenis = '"+Jenis+"'");
+        }
+    }
+}
